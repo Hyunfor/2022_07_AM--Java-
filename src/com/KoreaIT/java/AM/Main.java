bf93f9005aded77213ecfe55d44773c08c06743a
Hyunfor <navel5148@gmail.com>
2022-08-11 11:36:11 +0900

    명령어 입력 연습

--- src/com/KoreaIT/java/AM/Main.java
+++ src/com/KoreaIT/java/AM/Main.java
@@ -7,12 +7,15 @@ public class Main {
 		System.out.println("==프로그램 시작==");
 
 		Scanner sc = new Scanner(System.in);
-
-		System.out.printf("명령어) ");
-
-		String cmd = sc.nextLine();
-
-		System.out.printf("입력된 명령어 : %s\n", cmd);
+		
+		while(true) { // 명령어 무한 반복
+			System.out.printf("명령어) ");
+			String cmd = sc.nextLine(); // cmd는 사용자가 직접 입력하는것. - 키보드
+			
+			if(cmd.equals("exit")) { // 탈출구
+				break;
+			}
+		}
 
 		sc.close();
 
